05726c3c6c5e21e9848d6de05a586f8502cad264
statoshi <jameson@team.casa>
2019-06-05 15:35:14 -0400

    hello world

---
+++ js/bsp-uncompressed.js
@@ -0,0 +1,192 @@
+var unit = "BTC";
+
+$(function () {
+    updatePage();
+});
+
+$(".trigger").change(updatePage);
+
+$("#btc").change(function () {
+    unit = "BTC";
+    updatePage();
+});
+
+$("#mBtc").change(function () {
+    unit = "mBTC";
+    updatePage();
+});
+
+$("#btnFixed").click(function () {
+    var rake = parseFloat(cleanString($("#inputRake").val()));
+    $("#inputMultiplier").val(100 / (100 - rake));
+    updatePage();
+});
+
+function updatePage() {
+
+    validateInitial();
+    validateExchRate();
+    validateRake();
+    validateCycles();
+    validateMultiplier();
+    
+    $(".btc").text(unit);
+    
+    var initial = parseFloat(cleanString($("#inputInitial").val()));
+    var exchange = parseFloat(cleanString($("#inputExchRate").val()));
+    var rake = parseFloat(cleanString($("#inputRake").val()));
+    var cycles = parseInt(cleanString($("#inputCycles").val()), 10);
+    var multiplier = parseFloat(cleanString($("#inputMultiplier").val()));
+
+    $('#results').html('');
+
+    var conversionFactor = (unit == "mBTC") ? 1000 : 1;
+	
+	var remainingBtc = initial;
+	remainingBtc *= conversionFactor;
+	var initialBtcOrMbtc = remainingBtc;
+	var previousBtc = initialBtcOrMbtc;
+	
+    // First row
+    var row = $('<tr class="initial-row">');
+    row.append($('<td>').html(''));
+    row.append($('<td>').html(formatCurrency(exchange)));
+    row.append($('<td>').html(numberWithCommas(initialBtcOrMbtc.toFixed(2))));
+    row.append($('<td>').html(formatCurrency(initial * exchange)));
+    row.append($('<td>').html('0'));
+    row.append($('<td>').html(' '));
+    row.append($('<td>').html(' '));
+    row.append($('<td>').html('100.00%'));
+	$('#results').append(row);
+
+	var runningTotal = 0;
+
+    // Subsequent rows
+    for (var i = 0; i < cycles; i++) {
+        var row = $('<tr>');
+        
+		
+		btcCurrentValue = exchange * Math.pow(multiplier, i + 1);
+		remainingBtc = remainingBtc * (1 - rake / 100);
+		remainingBtcValue = btcCurrentValue * remainingBtc / conversionFactor
+	    var soldBtc = previousBtc - remainingBtc;
+		previousBtc = remainingBtc;
+		var soldValue = btcCurrentValue * soldBtc / conversionFactor;
+		runningTotal += soldValue;
+		
+        row.append($('<td>').html(i + 1));
+        row.append($('<td>').html(formatCurrency((btcCurrentValue).toFixed(2))));
+        row.append($('<td>').html(numberWithCommas(remainingBtc.toFixed(2))));
+        row.append($('<td>').html(formatCurrency((remainingBtcValue).toFixed(2))));
+        row.append($('<td>').html(soldBtc.toFixed(2)));
+        row.append($('<td>').html(formatCurrency((soldValue).toFixed(2))));
+        row.append($('<td>').html(formatCurrency((runningTotal).toFixed(2))));
+        row.append($('<td>').html((remainingBtcValue / (remainingBtcValue + runningTotal) * 100).toFixed(2) + '%'));
+
+        $('#results').append(row);
+    }
+
+    var data = $('#results').table2CSV({
+        delivery: 'value',
+        header: ['Cycle', '$/BTC', unit + ' Remaining', 'Value in $', unit + ' Sold', '$ Out', '$ Out Running Total', '% BTC']
+    });
+
+    if (navigator.userAgent.search("Chrome") >= 0) {
+        blob = new Blob([data], { type: 'text/csv' }); //new way
+        var csvUrl = URL.createObjectURL(blob);
+    }
+    else {
+        var csvUrl = 'data:text/csv;charset=UTF-8,' + encodeURIComponent(data);
+    }
+
+    $('#dtLink').empty();
+    $('#dtLink').append(
+        $('<a>')
+            .attr({
+                'href': csvUrl,
+                'download': 'bitcoinsavingsplan.csv'
+                }
+            ).html(
+                '<span class="glyphicon glyphicon-save"></span> Download data in .CSV format'
+                )
+            );
+
+}
+
+function validateInitial() {
+    var value = parseFloat(cleanString($("#inputInitial").val())).toFixed(2);
+    if (isNaN(value) || value <= 0)
+        value = 1;
+    $("#inputInitial").val(numberWithCommas(value));
+}
+
+function validateExchRate() {
+    var value = parseFloat(cleanString($("#inputExchRate").val())).toFixed(2);
+    if (isNaN(value) || value <= 0)
+        value = 1;
+    $("#inputExchRate").val(numberWithCommas(value));
+}
+
+function validateRake() {
+    var value = parseFloat(cleanString($("#inputRake").val())).toFixed(2);
+    if (isNaN(value) || value <= 0)
+        value = 10;
+    if (value > 100)
+        value = 100;
+    $("#inputRake").val(value);
+}
+
+function validateCycles() {
+    var value = parseInt(cleanString($("#inputCycles").val()), 10);
+    if (isNaN(value) || value < 0) {
+        value = 0;
+    } else if (value > 1000) {
+        value = 1000;
+    }
+    $("#inputCycles").val(value);
+}
+
+function validateMultiplier() {
+    var value = parseFloat(cleanString($("#inputMultiplier").val()));
+    if (isNaN(value) || value <= 1) {
+        value = 2;
+    } else if (value > 100) {
+        value = 100;
+    }
+    $("#inputMultiplier").val(value);
+}
+
+function cleanString(x) {
+    return x.replace(/[^\d\.\-\ ]/g, '');
+}
+
+function formatCurrency(x) {
+    return '$' + numberWithCommas(x);
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+
+/*
+     FILE ARCHIVED ON 08:22:24 Jun 02, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 19:05:38 Jun 05, 2019.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 134.719 (3)
+  esindex: 0.016
+  captures_list: 171.393
+  CDXLines.iter: 12.696 (3)
+  PetaboxLoader3.datanode: 152.551 (5)
+  exclusion.robots: 0.398
+  exclusion.robots.policy: 0.376
+  RedisCDXSource: 20.561
+  PetaboxLoader3.resolve: 69.577 (2)
+  load_resource: 155.941
+*/
---
+++ js/table2csv.js
@@ -0,0 +1,95 @@
+jQuery.fn.table2CSV = function (options) {
+    var options = jQuery.extend({
+        separator: ',',
+        header: [],
+        delivery: 'popup' // popup, value
+    },
+    options);
+
+    var csvData = [];
+    var headerArr = [];
+    var el = this;
+
+    //header
+    var numCols = options.header.length;
+    var tmpRow = []; // construct header avalible array
+
+    if (numCols > 0) {
+        for (var i = 0; i < numCols; i++) {
+            tmpRow[tmpRow.length] = formatData(options.header[i]);
+        }
+    } else {
+        $(el).filter(':visible').find('th').each(function () {
+            if ($(this).css('display') != 'none') tmpRow[tmpRow.length] = formatData($(this).html());
+        });
+    }
+
+    row2CSV(tmpRow);
+
+    // actual data
+    $(el).find('tr').each(function () {
+        var tmpRow = [];
+        $(this).filter(':visible').find('td').each(function () {
+            if ($(this).css('display') != 'none') tmpRow[tmpRow.length] = formatData($(this).html());
+        });
+        row2CSV(tmpRow);
+    });
+    if (options.delivery == 'popup') {
+        var mydata = csvData.join('\n');
+        return popup(mydata);
+    } else {
+        var mydata = csvData.join('\n');
+        return mydata;
+    }
+
+    function row2CSV(tmpRow) {
+        var tmp = tmpRow.join('') // to remove any blank rows
+        // alert(tmp);
+        if (tmpRow.length > 0 && tmp != '') {
+            var mystr = tmpRow.join(options.separator);
+            csvData[csvData.length] = mystr;
+        }
+    }
+    function formatData(input) {
+        // replace " with “
+        var regexp = new RegExp(/["]/g);
+        var output = input.replace(regexp, "“");
+        //HTML
+        var regexp = new RegExp(/\<[^\<]+\>/g);
+        var output = output.replace(regexp, "");
+        if (output == "") return '';
+        return '"' + output + '"';
+    }
+    function popup(data) {
+        var generator = window.open('', 'csv', 'height=400,width=600');
+        generator.document.write('<html><head><title>CSV</title>');
+        generator.document.write('</head><body >');
+        generator.document.write('<textArea cols=70 rows=15 wrap="off" >');
+        generator.document.write(data);
+        generator.document.write('</textArea>');
+        generator.document.write('</body></html>');
+        generator.document.close();
+        return true;
+    }
+};
+/*
+     FILE ARCHIVED ON 09:47:38 Jun 02, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 19:04:45 Jun 05, 2019.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 75.227 (3)
+  esindex: 0.011
+  captures_list: 137.775
+  CDXLines.iter: 19.973 (3)
+  PetaboxLoader3.datanode: 136.914 (5)
+  exclusion.robots: 0.406
+  exclusion.robots.policy: 0.392
+  RedisCDXSource: 14.473
+  PetaboxLoader3.resolve: 130.505 (2)
+  load_resource: 220.471
+*/
